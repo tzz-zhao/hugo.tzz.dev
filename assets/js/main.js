04b03ca5ec727f354675da414a481777fcf03d8d
tzz-zhao <tzz.zhao@gmail.com>
2022-04-30 00:27:26 +0900

    logo追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 import {config,dom,library} from '@fortawesome/fontawesome-svg-core';
-import {faDragon,faTag} from '@fortawesome/free-solid-svg-icons';
-library.add(faDragon,faTag);
+import {faTerminal,faCircle,faTag} from '@fortawesome/free-solid-svg-icons';
+library.add(faCircle,faTerminal,faTag);
 dom.i2svg();
