bac7b341e8d2c41378f81495f980d3aad4652bb5
tzz-zhao <tzz.zhao@gmail.com>
2022-05-16 02:15:27 +0900

    navbar修正

--- assets/js/fontawesome.ts
+++
@@ -1,4 +0,0 @@
-import {config,dom,library} from '@fortawesome/fontawesome-svg-core';
-import {faTerminal,faCircle,faTag,faAnglesUp} from '@fortawesome/free-solid-svg-icons';
-library.add(faCircle,faTerminal,faTag,faAnglesUp);
-dom.i2svg();
